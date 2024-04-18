ba56f7c1f000f60329fd6039b66cbc87502447d5
Nikolai Lønberg <niklonberg@gmail.com>
2024-04-18 13:36:22 +0200

    fix guessValidator logic

--- src/guessValidator.js
+++ src/guessValidator.js
@@ -1,30 +1,52 @@
 function guessValidator(guess, wordToGuess) {
-  if (!guess) return null;
+  // This constant is a placeholder that indicates we've successfully
+  // dealt with this character (it's correct, or misplaced).
+  const letterExistsChar = "✓";
 
-  let comparison = [];
-
-  // check for correct letter and position
-  for (let i = 0; i < wordToGuess.length; i++) {
-    const status = wordToGuess[i] === guess[i] ? "correct" : "incorrect";
-    comparison = [
-      ...comparison,
-      {
-        letter: guess[i],
-        status: status,
-      },
-    ];
+  if (!guess) {
+    return null;
   }
 
-  // check for missplaced letter
-  for (let i = 0; i < wordToGuess.length; i++) {
-    if (comparison[i].status === "incorrect") {
-      const letterToCheck = comparison[i].letter;
-      if (wordToGuess.indexOf(letterToCheck) !== -1)
-        comparison[i].status = "misplaced";
+  const guessChars = guess.toUpperCase().split("");
+  const wordToGuessChars = wordToGuess.split("");
+
+  const result = [];
+
+  // Step 1: Look for correct letters.
+  for (let i = 0; i < guessChars.length; i++) {
+    if (guessChars[i] === wordToGuessChars[i]) {
+      result[i] = {
+        letter: guessChars[i],
+        status: "correct",
+      };
+      wordToGuessChars[i] = letterExistsChar;
+      guessChars[i] = letterExistsChar;
     }
   }
 
-  return comparison;
+  // Step 2: look for misplaced letters. If it's not misplaced,
+  // it must be incorrect.
+  for (let i = 0; i < guessChars.length; i++) {
+    if (guessChars[i] === letterExistsChar) {
+      continue;
+    }
+
+    let status = "incorrect";
+    const misplacedIndex = wordToGuessChars.findIndex(
+      (char) => char === guessChars[i]
+    );
+    if (misplacedIndex >= 0) {
+      status = "misplaced";
+      wordToGuessChars[misplacedIndex] = letterExistsChar;
+    }
+
+    result[i] = {
+      letter: guessChars[i],
+      status,
+    };
+  }
+
+  return result;
 }
 
 export default guessValidator;
